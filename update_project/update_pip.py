231321c46f9b1942a5325f2e347c9a88b483ac4e
OleksandrMakarov <alexander.vmv@gmail.com>
2023-05-05 22:07:57 +0200

    formatting code & subprocess.run output

--- update_project/update_pip.py
+++ update_project/update_pip.py
@@ -7,24 +7,30 @@ import subprocess
 def update_pip_packages() -> None:
     print("Checking for pip updates...")
 
-    pip_version = subprocess.check_output(
-        [sys.executable, "-m", "pip", "--version"]).decode().split()[1]
+    pip_version = (
+        subprocess.check_output([sys.executable, "-m", "pip", "--version"])
+        .decode()
+        .split()[1]
+    )
 
     with urllib.request.urlopen("https://pypi.org/pypi/pip/json") as response:
         pip_latest_version = json.load(response)["info"]["version"]
 
     if pip_version != pip_latest_version:
         print(
-            f"Updating pip from version {pip_version} to version {pip_latest_version}...")
+            f"Updating pip from version {pip_version} to version {pip_latest_version}..."
+        )
         subprocess.check_call(
-            [sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+            [sys.executable, "-m", "pip", "install", "--upgrade", "pip"]
+        )
         print("Pip updated successfully.")
     else:
         print(f"Pip is already up-to-date (version {pip_version}).")
 
     print("Checking for available pip package updates...")
     outdated_packages = subprocess.check_output(
-        [sys.executable, "-m", "pip", "list", "--outdated", "--format=json"]).decode()
+        [sys.executable, "-m", "pip", "list", "--outdated", "--format=json"]
+    ).decode()
     packages_to_update = [pkg["name"] for pkg in json.loads(outdated_packages)]
 
     if not packages_to_update:
@@ -33,10 +39,15 @@ def update_pip_packages() -> None:
         print("Updating pip packages...")
         for package in packages_to_update:
             update_result = subprocess.run(
-                [sys.executable, "-m", "pip", "install", "--upgrade", package])
+                [sys.executable, "-m", "pip", "install", "--upgrade", package],
+                check=False,
+                capture_output=False,
+                text=True,
+            )
             if update_result.returncode == 0:
                 print(f"{package} updated successfully.")
             else:
                 print(
-                    f"Error updating {package}. Return code: {update_result.returncode}")
+                    f"Error updating {package}. Return code: {update_result.returncode}"
+                )
         print("Pip packages update completed.")
--- update_project/utils.py
+++ update_project/utils.py
@@ -6,8 +6,8 @@ def get_linux_distribution():
     return distro.id()
 
 
-def get_project_version() -> str:
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    version_file_path = os.path.join(current_directory, "version")
-    with open(version_file_path, "r", encoding="utf-8") as file:
-        return file.read().strip()
+# def get_project_version() -> str:
+#     current_directory = os.path.dirname(os.path.abspath(__file__))
+#     version_file_path = os.path.join(current_directory, "version")
+#     with open(version_file_path, "r", encoding="utf-8") as file:
+#         return file.read().strip()
